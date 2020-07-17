8a39cf39a0db2b7b86525b5138ce5edb34ec04ec
billpope <epop.bill@gmail.com>
2020-07-16 21:18:18 -0400

    social

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
 import { hot } from 'react-hot-loader/root';
 import React, { useState, useEffect } from 'react';
+import { SocialIcon } from 'react-social-icons';
 import './styles.css'
 
 function App(props) {
-    const [state, setState] = useState([])
+    const [item, setState] = useState([])
     useEffect(() => {
         fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=new_york_yankees', {
             method:'get'
         }).then(response => {
             return response.json();
         }).then(json => {
-            setState(json)
+            setState(json.teams[0])
         })
     }, []); 
     return (
         <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-            {(state.teams || []).map((item => 
-                <div style={{width:'20%'}} key={item.idTeam.toString()}>
+           
+                <div style={{width:'20%'}}>
                     <h1><a target='_blank' href={`http://${item.strWebsite}`}>{item.strTeam}</a></h1>
                     <img style={{margin:'0 27%'}} width={100} src={item.strTeamBadge} alt="logo" />
                     <ul>
-                        <li><a target='_blank' href={`https://${item.strTwitter}`}><img alt='twitter'/></a></li>
-                        <li><a target='_blank' href={`https://${item.strFacebook}`}><img alt='facebook' /></a></li>
-                        <li><a target='_blank' href={`https://${item.strInstagram}`}><img alt='instagram' /></a></li>
+                        <li><SocialIcon network="twitter" href={`https://${item.strTwitter}`} /></li>
+                        <li><SocialIcon network="facebook" href={`https://${item.strFacebook}`} /></li>
+                        <li><SocialIcon network="instagram" href={`https://${item.strInstagram}`} /></li>
                     </ul> 
                     <hr />
                     <h2 style={{padding:'0 15%'}}>{item.strStadium}</h2>
@@ -30,7 +31,7 @@ function App(props) {
                         <img style={{width:'99%'}} src={item.strStadiumThumb} alt='Yankee Stadium' />
                         <div style={{overflow:'hidden', width:'99%',height:'90px'}}><a href="">{item.strStadiumDescription}</a></div>
                     </div>
-                    <h3 style={{padding:'0 30%'}}>League Info</h3>
+                    <h3 style={{padding:'0 30%'}}>Team Info</h3>
                     <table>
                         <thead>
                             <th>
@@ -60,7 +61,7 @@ function App(props) {
                         </tbody>
                     </table>
                 </div>
-            ))}
+           
         </div>
     );
 }
